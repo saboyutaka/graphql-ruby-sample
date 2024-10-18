6baa7e00d2940b7d0864e35389019177a863b75e
saboyutaka <yutaka.paradigm.shift@gmail.com>
2024-10-18 12:30:41 +0900

    app名を変更

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ require "action_view/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Sample
+module GraphqlApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
